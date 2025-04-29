66bd43b25ec98ffc7790b596aeecf0272a8b9414
ernisto <joaovitordesouza707@gmail.com>
2025-04-29 19:37:48 -0300

    fix: type warns

--- src/init.lua
+++ src/init.lua
@@ -6,34 +6,29 @@ type dropRates<drop = any> = { [drop]: number }
 
 --// Module Functions
 function RandomOption.new<drop>(_dropRates: dropRates<drop>?)
-    
-    local dropRates = if _dropRates then table.clone(_dropRates) else {}
-    
+    local dropRates: dropRates<drop> = if _dropRates then table.clone(_dropRates) else {}
+
     --// Instance
-    local linearRates = {}
+    local linearRates: { { maxRate: number, value: drop } } = {}
     local totalRate = 0
     local self = {}
-    
+
     --// Methods
     function self:remove(value: drop, rate: number?)
-        
         rate = rate or dropRates[value]
+        assert(rate)
         local rightSide = false
-        
-        for _,info in linearRates do
-            
+
+        for _, info in linearRates do
             if not rightSide and info.value ~= value then continue end
             rightSide = true
-            
             info.maxRate -= rate
         end
         if rate then dropRates[value] -= rate else dropRates[value] = nil end
         return self
     end
-    function self:add(rate: number,...: drop)
-        
-        for _,value in {...} do
-            
+    function self:add(rate: number, ...: drop)
+        for _, value in { ... } do
             totalRate += rate
             dropRates[value] = rate + (dropRates[value] or 0)
             table.insert(linearRates, { value = value, maxRate = totalRate })
@@ -50,21 +45,18 @@ function RandomOption.new<drop>(_dropRates: dropRates<drop>?)
         end
         error('want possible choice')
     end
-    
+
     function self:getRarestDrops(amount: number)
-        
         local drops = {}
-        
+
         for _ = 1, amount do
-            
             local lowestRate = math.huge
             local rarestDrop
-            
+
             for drop, rate in dropRates do
-                
                 if table.find(drops, drop) then continue end
                 if rate > lowestRate then continue end
-                
+
                 lowestRate = rate
                 rarestDrop = drop
             end
@@ -72,11 +64,12 @@ function RandomOption.new<drop>(_dropRates: dropRates<drop>?)
         end
         return unpack(drops)
     end
-    function self:getWithLuck(multiplier: number,...: drop)
-        
+    function self:getWithLuck(multiplier: number, ...: drop)
         local buffedRates = table.clone(dropRates)
-        for _,drop in {...} do buffedRates[drop] *= multiplier end
-        
+        for _, drop in { ... } do
+            buffedRates[drop] *= multiplier
+        end
+
         return RandomOption.new(buffedRates)
     end
     
@@ -84,10 +77,12 @@ function RandomOption.new<drop>(_dropRates: dropRates<drop>?)
         
         return RandomOption.new(table.clone(dropRates))
     end
-    
+
     --// Setup
-    for value, rate in pairs(dropRates or {}) do self:add(rate, value) end
-    
+    for value, rate in dropRates do
+        self:add(rate, value)
+    end
+
     --// End
     return (self :: any) :: RandomOption<drop>
 end
@@ -95,9 +90,10 @@ export type RandomOption<drop = any> = {
     add: (any, rate: number, value: drop) -> RandomOption<drop>,
     remove: (any, value: drop, rate: number?) -> RandomOption<drop>,
     choice: (any) -> drop,
-    
+
     getRarestDrops: (any, amount: number) -> ...drop,
-    getWithLuck: (any, multiplier: number,...drop) -> RandomOption<drop>,
+    getWithLuck: (any, multiplier: number, ...drop) -> RandomOption<drop>,
+    clone: (any) -> RandomOption<drop>,
 }
 
 --// End
